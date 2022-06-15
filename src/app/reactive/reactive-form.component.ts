3ebaa3d0e87db0d64f9b3a09128487b5e260de2f
Filipp Pashkevich <fpashkevich@pstlabs.by>
2022-06-15 16:03:44 +0300

    fpashkevich: reactive form data model

--- src/app/reactive/reactive-form.component.ts
+++ src/app/reactive/reactive-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ChangeDetectionStrategy } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
   selector: 'reactive-form',
@@ -8,8 +9,19 @@ import { Component, OnInit, ChangeDetectionStrategy } from "@angular/core";
 })
 
 export class ReactiveFormComponent implements OnInit {
+  public form: FormGroup;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      name: new FormControl(''),
+      username: new FormControl('')
+    })
+  }
+
+  public processForm() {
+    console.log(this.form.value);
+  }
 
 }
