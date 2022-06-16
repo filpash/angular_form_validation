e8beea5ba7abb069ade4bbfa87ded6bc51737d0f
Filipp Pashkevich <fpashkevich@pstlabs.by>
2022-06-16 12:57:24 +0300

    fpashkevich: refactoring ngOnInit

--- src/app/reactive/reactive-form.component.ts
+++ src/app/reactive/reactive-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ChangeDetectionStrategy } from "@angular/core";
-import { FormGroup, FormControl, FormBuilder, Validators } from "@angular/forms";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 @Component({
   selector: 'reactive-form',
@@ -16,37 +16,37 @@ export class ReactiveFormComponent implements OnInit {
   constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.buildForm();
+  }
+
+  public buildForm() {
+    // build our form
     this.form = this.fb.group({
       name: ['', [Validators.minLength(3), Validators.maxLength(6)]],
       username: ['', [Validators.required, Validators.minLength(3)]]
     });
 
-    this.form.valueChanges.subscribe(data => {
-      this.nameError = '';
-      this.usernameError = '';
+    // watch for changes & validate
+    this.form.valueChanges.subscribe(() => this.validateForm());
+  }
 
-      let name = this.form.get('name');
-      let username = this.form.get('username');
+  public validateForm() {
+    this.nameError = '';
+    this.usernameError = '';
 
-      if (name?.invalid && name.dirty) {
-        if (name?.errors['required'])
-          this.nameError = 'Name is required.'
+    let name = this.form.get('name');
+    let username = this.form.get('username');
 
-        if (name?.errors['minlength'])
-          this.nameError = 'Name must be at least  3 characters.'
+    if (name?.invalid && name.dirty) {
+      if (name?.errors['required']) this.nameError = 'Name is required.'
+      if (name?.errors['minlength']) this.nameError = 'Name must be at least  3 characters.'
+      if (name?.errors['maxlength']) this.nameError = 'Name can\'t be more than 6 characters.'
+    }
 
-        if (name?.errors['maxlength'])
-          this.nameError = 'Name can\'t be more than 6 characters.'
-      }
-
-      if (username?.invalid && username.dirty) {
-        if (username?.errors['required'])
-          this.usernameError = 'Username is required.'
-
-        if (username?.errors['minlength'])
-          this.usernameError = 'Username must be at least  3 characters.'
-      }
-    })
+    if (username?.invalid && username.dirty) {
+      if (username?.errors['required']) this.usernameError = 'Username is required.'
+      if (username?.errors['minlength']) this.usernameError = 'Username must be at least  3 characters.'
+    }
   }
 
   public processForm() {
